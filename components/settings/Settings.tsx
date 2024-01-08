ad2592fa2cd6340386a78570dd917687280016a4
sliu <48802197+lambdasc@users.noreply.github.com>
2024-01-07 19:02:04 -0600

    fix case

--- components/settings/Settings.tsx
+++ components/settings/Settings.tsx
@@ -1,6 +1,7 @@
 import EditStyles from "@/components/settings/EditStyles";
 
-return (
+export default function Settings() {
+    return (
         <div className="p-4">
             <p className="text-sm text-slate-600 mb-2">Settings are saved locally on your browser.</p>
             <EditStyles />
